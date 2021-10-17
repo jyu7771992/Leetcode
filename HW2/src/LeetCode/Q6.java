26a7e8c5e2cbeb1d910632e1015febc2364b495f
jyu7771992 <jyu7771992@gmail.com>
2021-10-16 21:13:55 -0700

    Debug Q6 + Q7

--- HW2/src/LeetCode/Q6.java
+++ HW2/src/LeetCode/Q6.java
@@ -1,9 +1,40 @@
 package LeetCode;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Q6 {
+    public static boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map1 = new HashMap<>();
+        Map<Character,Character> map2 = new HashMap<>();
+        char x,y;
+        for(int i=0; i<s.length(); i++){
+            x = s.charAt(i);
+            y = t.charAt(i);
+            if(map1.containsKey(x)){
+                if(y != map1.get(x)){
+                    return false;
+                }
+            } else {
+                map1.put(x,y);
+            }
+
+            if (map2.containsKey(y)){
+                if(x != map2.get(y)){
+                    return false;
+                }
+            } else {
+                map2.put(y,x);
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         System.out.println("---------(Example 1)-----------");
+        System.out.println(isIsomorphic("egg","add"));
         System.out.println("---------(Example 2)-----------");
-
+        System.out.println(isIsomorphic("eat","tss"));
+        System.out.println("---------(Example 3)-----------");
+        System.out.println(isIsomorphic("aas","add"));
     }
 }
--- HW2/src/LeetCode/Q7.java
+++ HW2/src/LeetCode/Q7.java
@@ -1,9 +1,28 @@
 package LeetCode;
 
 public class Q7 {
+    public static String addStrings(String num1, String num2) {
+        String ans = "";
+        int carry = 0;
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+
+        while(i >= 0 || j >= 0 || carry != 0){
+            int i_val= i>=0? num1.charAt(i)-'0':0;
+            int j_val= j>=0? num2.charAt(j)-'0':0;
+            i--; j--;
+            int sum = i_val + j_val + carry;
+            carry = sum/10;
+            ans = sum % 10 + ans;
+        }
+        return ans;
+    }
     public static void main(String[] args) {
         System.out.println("---------(Example 1)-----------");
+        System.out.println(addStrings("123","34"));
         System.out.println("---------(Example 2)-----------");
-
+        System.out.println(addStrings("123","99"));
+        System.out.println("---------(Example 3)-----------");
+        System.out.println(addStrings("123222","999"));
     }
 }
